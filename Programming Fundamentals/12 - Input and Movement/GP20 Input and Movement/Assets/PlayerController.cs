e6690753ea94d87dbfcc27c57cf051b4a32fd20b
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-14 21:36:33 +0200

    Add assignment 12

---
+++ Programming Fundamentals/12 - Input and Movement/GP20 Input and Movement/Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour {
+	private enum MovementMethod { Translate, AddForce, Velocity, MovePosition }
+
+	[SerializeField] private MovementMethod movementMethod;
+	[SerializeField] private float movementSpeed;
+
+	private new Rigidbody2D rigidbody;
+
+
+	private void Awake() {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() {
+		if (movementMethod == MovementMethod.Translate)
+			TranslateMovement();
+		else if (movementMethod == MovementMethod.Velocity)
+			VelocityMovement();
+	}
+
+	private void FixedUpdate() {
+		if (movementMethod == MovementMethod.AddForce)
+			AddForceMovement();
+		else if (movementMethod == MovementMethod.MovePosition)
+			MovePositionMovement();
+	}
+
+
+	private void TranslateMovement() {
+		transform.Translate(GetInputVector() * (movementSpeed * Time.deltaTime));
+	}
+
+	private void AddForceMovement() {
+		rigidbody.AddForce(GetInputVector() * movementSpeed);
+	}
+
+	private void VelocityMovement() {
+		rigidbody.velocity = GetInputVector() * movementSpeed;
+	}
+
+	private void MovePositionMovement() {
+		rigidbody.MovePosition(rigidbody.position + GetInputVector() * (movementSpeed * Time.deltaTime));
+	}
+
+
+	private static Vector2 GetInputVector() {
+		Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		if (inputVector.sqrMagnitude > 1)
+			inputVector.Normalize();
+
+		return inputVector;
+	}
+}
---
+++ Programming Fundamentals/12 - Input and Movement/README.md
@@ -0,0 +1,3 @@
+# Input and Movement (in Unity)
+
+![](GP20%20Input%20and%20Movement/demo.gif)
