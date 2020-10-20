f5e3520f3903f62deb48bfeabfed4374c0b801c1
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-20 15:45:44 +0200

    Add assignment 13

---
+++ Programming Fundamentals/13 - Physics/Basketball/Assets/Scripts/Hoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hoop : MonoBehaviour {
+	[SerializeField] private Text scoreText;
+
+	private int score;
+
+	private void Start() {
+		UpdateScoreText();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.attachedRigidbody.velocity.y < 0 && other.CompareTag("Ball")) {
+			score++;
+			UpdateScoreText();
+		}
+	}
+
+	private void UpdateScoreText() {
+		scoreText.text = $"Score: {score}";
+	}
+}
---
+++ Programming Fundamentals/13 - Physics/Basketball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] private float speed;
+	[SerializeField] private float jumpForce;
+	[SerializeField] private Collider2D groundTrigger;
+
+	private new Rigidbody2D rigidbody;
+
+	private float moveInput;
+	private bool jumpInput;
+	private bool isGrounded;
+
+	private void Awake() {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() {
+		moveInput = Input.GetAxis("Horizontal");
+
+		if (Input.GetButtonDown("Jump") && isGrounded) {
+			jumpInput = true;
+		}
+	}
+
+	private void FixedUpdate() {
+		Vector2 velocity = rigidbody.velocity;
+		velocity.x = moveInput * speed;
+
+		if (jumpInput) {
+			velocity.y += jumpForce;
+			jumpInput = false;
+		}
+
+		rigidbody.velocity = velocity;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) => UpdateGrounded();
+	private void OnTriggerExit2D(Collider2D other) => UpdateGrounded();
+
+	private void UpdateGrounded() {
+		isGrounded = groundTrigger.IsTouchingLayers();
+	}
+}
